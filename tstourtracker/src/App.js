da9da63bc3ad06c4db2f49e17423ec8da099a747
Mattie <mrm4@iastate.edu>
2024-05-17 18:05:18 -0500

    fix view on phones for about page

--- tstourtracker/src/App.js
+++ tstourtracker/src/App.js
@@ -121,7 +121,7 @@ function App() {
             <div style={{ paddingTop: '100px' }}>
                 <footer class="container">
                     <p class="float-end"><a href="#">Back to top</a></p>
-                    <p class="col-md-4 mb-0 text-body-secondary"><button class="nav-link px-2 text-body-secondary" onClick={() => clickNavButton(7)}>&copy; 2024 TS Tour Tracker</button></p>
+                    <p class="col-md-4 mb-0 text-body-secondary"><button class="nav-link px-2 text-body-secondary" onClick={() => setView(7)}>&copy; 2024 TS Tour Tracker</button></p>
                 </footer>
             </div>
         );
@@ -1855,45 +1855,91 @@ function App() {
     }
 
     function viewAbout() {
-        return (
-            <div>
-                {navbar()}
-                <div class="container">
-                    <h1 class="page-title">About</h1>
-                    <hr class="featurette-divider" />
-                    <div class="row">
-                        <div class="col aboutUs">
-                            <p class="title"><strong>About TS Tour Tracker</strong></p>
-                            <p>TS Tour Tracker is a website designed to track what's happening during Taylor Swift's Eras Tour.
-                                The website was designed to replace my spreadsheet because my family had trouble viewing it on their phones.</p>
-                            <p>The website tracks the surprise songs, which songs in Taylor's discography have and have not been played,
-                                what songs are on the main setlist, what outfits Taylor wears, and any events that Taylor might attend.
-                                The events include concerts, song releases, award ceremonies, and football games.
-                                There is also a page with links to other important websites, such as tour streams, other trackers,
-                                and Taylor's official social media and websites.</p>
-                        </div>
-                        <div class="col aboutWillow">
-                            <div class="row">
-                                <p class="title willowTitle"><strong>About Willow</strong></p>
-                            </div>
-                            <div class="row">
-                                <div class="col">
-                                    <img src={willowPic} className="featurette-image img-fluid mx-auto" style={{ width: '300px', height: '300px', objectFit: 'cover', margin: '10px' }} alt="A black dog sitting in front of a double rainbow" />
-                                </div>
-                                <div class="col">
-                                    <p>This is my puppy Willow! She is a 3 year old black lab and a very silly little lady.
-                                        She loves playing with rope toys and tennis balls, going on walks, and causing chaos.
-                                        She is very proud to now have two Taylor Swift songs about her,
-                                        "willow" and "The Black Dog."  </p>
-                                </div>
-                            </div>
+        const isSmallScreen = window.innerWidth <= 800;
 
+        if (isSmallScreen) {
+            return (
+                <div>
+                    {navbar()}
+                    <div class="container">
+                        <h1 class="page-title">About</h1>
+                        <hr class="featurette-divider" />
+                        <div class="row">
+                            <div class="col aboutUs">
+                                <p class="title"><strong>About TS Tour Tracker</strong></p>
+                                <p>TS Tour Tracker is a website designed to track what's happening during Taylor Swift's Eras Tour.
+                                    The website was designed to replace my spreadsheet because my family had trouble viewing it on their phones.</p>
+                                <p>The website tracks the surprise songs, which songs in Taylor's discography have and have not been played,
+                                    what songs are on the main setlist, what outfits Taylor wears, and any events that Taylor might attend.
+                                    The events include concerts, song releases, award ceremonies, and football games.
+                                    There is also a page with links to other important websites, such as tour streams, other trackers,
+                                    and Taylor's official social media and websites.</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col aboutWillow">
+                                <div class="row">
+                                    <p class="title willowTitle"><strong>About Willow</strong></p>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <img src={willowPic} className="featurette-image img-fluid mx-auto" style={{ width: '300px', height: '300px', objectFit: 'cover', margin: '10px' }} alt="A black dog sitting in front of a double rainbow" />
+                                    </div>
+                                    <div class="col">
+                                        <p>This is my puppy Willow! She is a 3 year old black lab and a very silly little lady.
+                                            She loves playing with rope toys and tennis balls, going on walks, and causing chaos.
+                                            She is very proud to now have two Taylor Swift songs about her,
+                                            "willow" and "The Black Dog."  </p>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
+                    {footer()}
+                </div >
+            );
+        } else {
+            return (
+                <div>
+                    {navbar()}
+                    <div class="container">
+                        <h1 class="page-title">About</h1>
+                        <hr class="featurette-divider" />
+                        <div class="row">
+                            <div class="col aboutUs">
+                                <p class="title"><strong>About TS Tour Tracker</strong></p>
+                                <p>TS Tour Tracker is a website designed to track what's happening during Taylor Swift's Eras Tour.
+                                    The website was designed to replace my spreadsheet because my family had trouble viewing it on their phones.</p>
+                                <p>The website tracks the surprise songs, which songs in Taylor's discography have and have not been played,
+                                    what songs are on the main setlist, what outfits Taylor wears, and any events that Taylor might attend.
+                                    The events include concerts, song releases, award ceremonies, and football games.
+                                    There is also a page with links to other important websites, such as tour streams, other trackers,
+                                    and Taylor's official social media and websites.</p>
+                            </div>
+                            <div class="col aboutWillow">
+                                <div class="row">
+                                    <p class="title willowTitle"><strong>About Willow</strong></p>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <img src={willowPic} className="featurette-image img-fluid mx-auto" style={{ width: '300px', height: '300px', objectFit: 'cover', margin: '10px' }} alt="A black dog sitting in front of a double rainbow" />
+                                    </div>
+                                    <div class="col">
+                                        <p>This is my puppy Willow! She is a 3 year old black lab and a very silly little lady.
+                                            She loves playing with rope toys and tennis balls, going on walks, and causing chaos.
+                                            She is very proud to now have two Taylor Swift songs about her,
+                                            "willow" and "The Black Dog."  </p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    {footer()}
                 </div>
-                {footer()}
-            </div>
-        )
+            );
+        }
     }
 
 
